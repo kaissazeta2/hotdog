dad16a0d570efdc7955714c4770fc57c5c653f00
Kate <166900119+kaissazeta2@users.noreply.github.com>
2025-03-17 18:53:43 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int age(int a) {
+    if (a < 12) { return 5; }
+    else if (a >= 12 && a<=60) { return 10; }
+    else { return 7; }
+}
+double time(double b, int c) {
+    if (b <= 12) { return c*0.8;}
+    else { return c; }
+}
+int main()
+{
+    int k;
+    double l;
+    cin >> k >> l;
+    cout << time(l, age(k));
+    
+}
+
---
+++ seed.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+using namespace std;
+
+bool isPrime(int a) {
+    for (int i = 2; i <= sqrt(a); i++) {
+        if (a % i == 0) { return 0; }
+    }
+    return a!=1;
+}
+void razlozhenie(int z) {
+    if (z == 1) { cout << 1; }
+    else {
+        for (int w = 2; w <= z; w++) {
+        while (z % w == 0) {
+            z /= w;
+            cout << w << endl;
+            }
+        }
+    }
+}
+int nod(int a, int b, int c=1) {
+    for (int w = 2; w <= b; w++) {
+        while (a % w == 0 && b%w==0) {
+            a /= w;
+            b /= w;
+            c *= w;
+        }
+    }
+    return c;
+}
+int main()
+{
+    int x,y;
+    cin >> x>>y;
+    if (x <= y) {
+        y = x - y;
+        x = x - y;
+        y = y + x;
+    }
+    cout << nod(x, y) << endl << x * y / (nod(x, y));
+    
+    
+
+}
+
+
